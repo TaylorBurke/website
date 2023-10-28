23a371b5bcfc96ca10632b7322331295ca86f6a0
Taylor Burke <taylorburke@Taylors-MBP.lan>
2023-10-28 12:54:40 -0600

    some basic shtuff

--- website/src/App.js
+++ website/src/App.js
@@ -1,22 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import portrait from './image/portrait.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img className="heroImage" src={portrait}/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Developer + <br/>Consultant.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
       </header>
     </div>
   );
