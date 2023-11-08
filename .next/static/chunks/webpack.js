fc15d1b7ba0f459ad821d148645baed9fa876d6c
Taylor Burke <taylorburke@Taylors-MBP.lan>
2023-11-07 20:40:12 -0700

    fix background property

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "168a4bdd17c34ba2"; }
+/******/ 		__webpack_require__.h = function() { return "048ba0e197c2b452"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
