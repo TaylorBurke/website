f5bc8b61ff6ec2dd97bc17d301554aa9e447c447
Taylor Burke <taylorburke@Taylors-MBP.lan>
2023-11-08 19:02:30 -0700

    more style tweak

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a0c8d9016fef6257"; }
+/******/ 		__webpack_require__.h = function() { return "09f3fbb226c502a8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
