1624e2b599a721b2c918bfba1a4bc4e5b17ea523
Taylor Burke <taylorburke@Taylors-MBP.lan>
2023-11-09 16:03:31 -0700

    more configs

---
+++ out/_next/static/ORLt3WlmjoQeA_A_hj03w/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/902-71ae688175c81afa.js","static/chunks/pages/index-8e787b154dfeb421.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
